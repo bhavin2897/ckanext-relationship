d710939708e84770632894c3e65d9af4efe35355
Bhavin Katabathuni <bhavin2897@gmail.com>
2025-06-20 15:11:21 +0200

    updated multiple_value,  measurement_technique_proxy tokenization.

--- ckanext/relationship/plugin.py
+++ ckanext/relationship/plugin.py
@@ -133,11 +133,11 @@ class RelationshipPlugin(plugins.SingletonPlugin):
 
                             if techniques:
                             # Add a virtual field for indexing only
-                                pkg_dict['measurement_technique_proxy'] = techniques
+                                pkg_dict['measurement_technique_proxy'] = techniques # testing
 
                             if repository:
                                 repository_proxy.append(repository)
-                                pkg_dict['organization_proxy']= repository_proxy
+                                pkg_dict['organization_proxy']= repository_proxy # testing
                                 log.debug(f'related {repositroy}')
 
                         except Exception as e:
